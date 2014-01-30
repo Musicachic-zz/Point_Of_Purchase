74e89adb3a0d498c6165a6afa52581709ad3aef8
Musicachic <Musicachic@gmail.com>
2014-01-30 18:28:19 -0500

    Stubbed out more of the Login class for the prototype.

--- src/pottst1/data/Login.java
+++ src/pottst1/data/Login.java
@@ -17,45 +17,64 @@ public class Login
 /* This method is for getting the input of the username from the user. Eventually I will need a validator class to call
 to verify that the username is 6-12 alphanumeric characters long as per the spec document.*/
 
-    private static String main(String[] args)
+    private static String Username(String[] args)
     {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter your username: ");
         String username = sc.next();
 
+
+        //Will use this boolean more when actually validating the password.
         boolean isValid = false;
 
+
+        //Change to !isValid once you have more of a list to compare to.
         while (!isValid)
         {
 
-            String raw_input = sc.nextLine();
-
-            try
+            if (!username.equalsIgnoreCase("employee") || username.equals(""))
             {
 
+                System.out.println("Error! Username is required. Try again.");
             }
+            else
+            {
+                isValid = true;
+            }
+
         }
+        return username;
     }
 
 /* This method is for getting the input of the password from the user. We want to use char so the password cannot
    easily stolen as a security feature. Eventually we want to validate the password is correct and meets the specs
    doc of 8-20 characters long including letters, numbers, and special characters. */
 
-    public static char main(String[] args)
+    public static String Password(String[] args)
     {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter your password: ");
-        char password = sc.next();
+        String password = sc.next();
 
         boolean isValid = false;
 
         while (!isValid)
         {
+            if (password.equals("")){
+                System.out.println("Error! Password is required. Try again.");
+            }
+            if (!password.equalsIgnoreCase("password")){
 
+            System.out.println("Error! Password is invalid. Try again.");
         }
+            else {
+                isValid = true;
+            }
+        }
+        return password;
     }
 
 }
