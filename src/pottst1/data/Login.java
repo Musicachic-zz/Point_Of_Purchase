d9c58228b6a3abcece08bb89c1cf0694421c2ecc
Musicachic <Musicachic@gmail.com>
2014-01-30 18:47:25 -0500

    Stubbed out more of the Login class for the prototype.

--- src/pottst1/data/Login.java
+++ src/pottst1/data/Login.java
@@ -2,7 +2,7 @@ package pottst1.data;
 
 /*         ====================================================
 *          # Name: Teresa Potts (POTTST1)
-*          # Date: 1/28/14: 
+*          # Date: 1/30/14:
 *          # Course Name: CITP 290
 *          # Description: 
 *          ====================================================
@@ -17,64 +17,72 @@ public class Login
 /* This method is for getting the input of the username from the user. Eventually I will need a validator class to call
 to verify that the username is 6-12 alphanumeric characters long as per the spec document.*/
 
-    private static String Username(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
+	private static String username(String[] args)
+	{
 
-        System.out.print("Enter your username: ");
-        String username = sc.next();
+		//Will use this boolean more when actually validating the password.
+		boolean isValid = false;
 
+		String username = "";
+		Scanner sc = new Scanner(System.in);
 
-        //Will use this boolean more when actually validating the password.
-        boolean isValid = false;
+		//Change to !isValid once you have more of a list to compare to.
+		while (!isValid)
+		{
 
+			System.out.print("Enter your username: ");
+			username = sc.next();
 
-        //Change to !isValid once you have more of a list to compare to.
-        while (!isValid)
-        {
+			if (username.equals(""))
+			{
+				System.out.println("Error! Username is required. Try again.");
+			}
+			if (!username.equalsIgnoreCase("employee"))
+			{
+				System.out.println("Error! Username doesn't exist. Try again.");
+			}
+			else
+			{
+				isValid = true;
+			}
 
-            if (!username.equalsIgnoreCase("employee") || username.equals(""))
-            {
-
-                System.out.println("Error! Username is required. Try again.");
-            }
-            else
-            {
-                isValid = true;
-            }
-
-        }
-        return username;
-    }
+		}
+		return username;
+	}
 
 /* This method is for getting the input of the password from the user. We want to use char so the password cannot
    easily stolen as a security feature. Eventually we want to validate the password is correct and meets the specs
    doc of 8-20 characters long including letters, numbers, and special characters. */
 
-    public static String Password(String[] args)
-    {
+	public static String password(String[] args)
+	{
 
-        Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter your password: ");
-        String password = sc.next();
+		String password = "";
 
-        boolean isValid = false;
+		boolean isValid = false;
 
-        while (!isValid)
-        {
-            if (password.equals("")){
-                System.out.println("Error! Password is required. Try again.");
-            }
-            if (!password.equalsIgnoreCase("password")){
+		while (!isValid)
+		{
+			System.out.print("Enter your password: ");
+			password = sc.next();
 
-            System.out.println("Error! Password is invalid. Try again.");
-        }
-            else {
-                isValid = true;
-            }
-        }
-        return password;
-    }
+			if (password.equals(""))
+			{
+				System.out.println("Error! Password is required. Try again.");
+			}
+			if (!password.equalsIgnoreCase("password"))
+			{
+
+				System.out.println("Error! Password is invalid. Try again.");
+			}
+			else
+			{
+				isValid = true;
+			}
+		}
+		return password;
+	}
 
 }
